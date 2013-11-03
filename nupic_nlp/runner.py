4b8b60c7157067a1073bce53ecd511e1b9750725
Matthew Taylor <matt@numenta.org>
2013-11-03 15:23:24 -0800

    changing how i'm saving sdr images

--- nupic_nlp/runner.py
+++ nupic_nlp/runner.py
@@ -4,6 +4,11 @@ import os
 import numpy
 import matplotlib.pyplot as plt
 
+red =   [1., 0., 0.]
+blue =  [0., 0., 1.]
+green = [0., 1., 0.]
+
+
 def read_words_from(file):
   lines = open(file).read().strip().split('\n')
   return [tuple(line.split(',')) for line in lines]
@@ -11,6 +16,8 @@ def read_words_from(file):
 def strip_punctuation(s):
   return s.translate(string.maketrans("",""), string.punctuation)
 
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+  return ''.join(choice(chars) for x in range(size))
 
 class Sparsity_Exception(Exception):
   pass
@@ -26,6 +33,9 @@ class Association_Runner(object):
     self.prediction_start = prediction_start
     self.output_dir = output_dir
     self.verbosity = verbosity
+    self.all_first_terms = []
+    self.all_second_terms = []
+
 
 
   def associate(self, pairs):
@@ -61,11 +71,11 @@ class Association_Runner(object):
 
 
   def random_dual_association(self, term1_file, term2_file):
-    all_first_terms = open(term1_file).read().strip().split('\n')
-    all_second_terms = open(term2_file).read().strip().split('\n')
+    self.all_first_terms = [t.lower() for t in open(term1_file).read().strip().split('\n')]
+    self.all_second_terms = [t.lower() for t in open(term2_file).read().strip().split('\n')]
     associations = []
     for count in range(0, self.max_terms):
-      associations.append((choice(all_first_terms), choice(all_second_terms)))
+      associations.append((choice(self.all_first_terms), choice(self.all_second_terms)))
     self.associate(associations)
 
 
@@ -81,39 +91,50 @@ minimum sparsity threshold of %.1f%%.' % (term, sparsity, self.min_sparsity))
     # print 'Sparsity %s:prediction ==> %.2f%%: %.2f%%' % (term, raw_sdr['sparsity'], output_sparsity)
     if fetch_word_from_sdr:
       if len(predicted_bitmap) is 0:
-        predicted_word = ' '
+        predicted_word = None
       else:
         predicted_word = self.builder.closest_term(predicted_bitmap)
       if (predicted_word is not None 
           and self.output_dir is not None 
           and len(predicted_bitmap) > 0):
-        # self._bitmap_to_image(predicted_bitmap, os.path.join(self.output_dir, predicted_word + '.png'))
-        self._generage_cept_bitmap_comparison_for_term(predicted_word, predicted_bitmap, self.output_dir);
+        self._generage_cept_bitmap_comparison_for_term(
+          predicted_word, 
+          predicted_bitmap, 
+          self.output_dir
+        );
+      elif len(predicted_bitmap) > 0:
+        # write unknown sdr to file
+        self._bitmap_to_image(
+          predicted_bitmap, 
+          os.path.join(self.output_dir, ('%s_predicted.png' % id_generator())), 
+          green
+        )
       return predicted_word
     else:
       return None
 
 
   def _generage_cept_bitmap_comparison_for_term(self, term, predicted_bitmap, path):
-    predicted_image = os.path.join(path, ('%s_predicted.png' % term))
+    predicted_image = os.path.join(path, ('%s_predicted_%s.png' % (term,id_generator())))
     cept_image = os.path.join(path, ('%s_cept.png' % term))
     cept_sdr = self.builder.term_to_sdr(term)
-    self._bitmap_to_image(predicted_bitmap, predicted_image)
-    self._bitmap_to_image(cept_sdr['positions'], cept_image)
+    self._bitmap_to_image(predicted_bitmap, predicted_image, red)
+    self._bitmap_to_image(cept_sdr['positions'], cept_image, blue)
 
 
-  def _bitmap_to_image(self, bitmap, path):
-    narr = self._bitmap_to_numpy_array(bitmap, 128);
+  def _bitmap_to_image(self, bitmap, path, rgb):
+    narr = self._bitmap_to_numpy_array(bitmap, 128, rgb);
     plt.imshow(narr);
     if self.verbosity > 0:
       print 'Saving image at %s' % path
     plt.savefig(path);
 
 
-  def _bitmap_to_numpy_array(self, bitmap, dimension):
-    narr = numpy.zeros((dimension, dimension), dtype="float32")
+  def _bitmap_to_numpy_array(self, bitmap, dimension, rgb=[0.,0.,0.]):
+    narr = numpy.ones((dimension, dimension, 3), dtype="float32")
     for index in bitmap:
       row = index / dimension
       col = index % dimension
-      narr[row, col] = 1
+      narr[row][col] = numpy.array(rgb, dtype="float32")
+
     return narr

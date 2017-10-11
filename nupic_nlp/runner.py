020eeb83ac67389ba80b6204a3385bf045c22b67
Matthew Taylor <matt@numenta.org>
2017-10-11 11:09:19 -0700

    print line

--- nupic_nlp/runner.py
+++ nupic_nlp/runner.py
@@ -48,6 +48,7 @@ class AssociationRunner(object):
 
 
   def associate(self, pairs):
+    print pairs
     print 'Prediction output for %i pairs of terms' % len(pairs)
     print '\n#%5s%16s%16s |%20s' % ('COUNT', 'TERM ONE', 'TERM TWO', 'TERM TWO PREDICTION')
     print '--------------------------------------------------------------------'
